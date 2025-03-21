0450b1c58a0a8b5c35e46fdd1ff4c9e461a0a085
devoil <devdevoildev@gmail.com>
2025-03-21 09:35:37 +0900

    Remove: 파일 삭제

--- node-textbook/3.node/3-4.node-built-in-objects/exit.js
+++
@@ -1,9 +0,0 @@
-let i = 1;
-setInterval(() => {
-  if (i === 5) {
-    console.log('종료!');
-    process.exit(); // 에러가 발생해서 종료하는 경우에는 process.exit(1); 이렇게 인자에 1을 넣어주면 됨
-  }
-  console.log(i);
-  i += 1;
-}, 1000);
