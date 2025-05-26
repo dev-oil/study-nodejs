15e59d22987df592f6ee6f50ab37c9572105e16a
devoil <devdevoildev@gmail.com>
2025-05-26 16:16:57 +0900

    feat: [node-basic] get과 send / sendFile

--- node-basic/server.js
+++ node-basic/server.js
@@ -3,3 +3,18 @@ const app = express();
 const port = 8080;
 
 app.listen(port, () => console.log('포트 8080으로 서버 대기중 ...'));
+
+// send 방식
+// app.get('/', (req, res) =>
+//   res.send(`<html>
+//       <body>
+//         <h1>HOMEPAGE</h1>
+//         <p>홈페이지입니다.</p>
+//         <marquee>dev-oil 님아 방가방가 ~~ ^^**</marquee>
+//       </body>
+//     </html>`)
+// );
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+app.get('/book', (req, res) => res.send('도서 목록 관련 페이지입니다.')); // req - request 요청정보, res - response 응답정보
